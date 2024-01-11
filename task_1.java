18e1290114700c19b8c98f0b1bd01ad2887cd088
Vikas Santosh Rathod <132924748+vikasrathod163@users.noreply.github.com>
2024-01-11 11:31:49 +0530

    Update task_1.java
    additional features added...

--- task_1.java
+++ task_1.java
@@ -3,33 +3,56 @@ import java.util.Scanner;
 public class task_1{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int min = 0;
+		int min = 1;
 		int max = 100;
-		int random_num = (int)(Math.random()*(max-min+1)+min); ;
-		//System.out.println(b);
-		  System.out.println("Guess The Number Between 0 to 100 : ");
-		  int attemtps = 0;
-		  boolean flag = true;
-		  
-		  while(flag){
-		  System.out.print("Enter The Number : ");
-		  int num = sc.nextInt();
-		  
-		  if(num == random_num){
-		  	attemtps++;
-		  	System.out.println("Right Guess......");
-		  	System.out.println("Total Attemps : "+attemtps);
-		  	flag = false;
-		  	
-		  }
-		  else if(num > random_num){
-		  	attemtps++;
-		  	System.out.println("Your entered number is Larger......");
-		  }
-		  else{
-		  	attemtps++;
-		  	System.out.println("Your entered number is Smaller .....");
-		  }
-		  }
+		int i = 0;
+		int limit = 10;
+		int wins = 0;
+		
+		System.out.println("Enter the no of rounds.... : ");
+		int round = sc.nextInt();
+
+		while (i < round) {
+			System.out.println("Round : " + (i + 1));
+			int random_num = (int)(Math.random() * (max - min + 1) + min);
+
+			//System.out.println(b);
+			System.out.println("Guess The Number Between 1 to 100 : ");
+			int attemtps = 0;
+			boolean flag = true;
+
+			while (flag) {
+				while (attemtps < limit) {
+					System.out.print("Enter The Number : ");
+					int num = sc.nextInt();
+
+					if (num == random_num) {
+						attemtps++;
+						System.out.println("Right Guess......");
+						System.out.println("Total Attemps : " + attemtps);
+						wins++;
+
+						flag = false;
+						break;
+					} else if (num > random_num) {
+						attemtps++;
+						System.out.println("too high .....");
+					} else {
+						attemtps++;
+						System.out.println("too low .....");
+					}
+				}
+				if (attemtps == limit && flag == true) {
+					System.out.println("You Lost...limit for attempts was " + limit);
+					flag = false;
+					break;
+				}
+
+			}
+			i++;
+		}
+
+		System.out.println("You have won " + wins + " Round out of " + round + " Rounds");
+
 	}
 }
