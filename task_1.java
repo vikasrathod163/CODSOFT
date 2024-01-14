41a3812dc1fbf64d583cc3c8284c0df83fa9dff4
Vikas Santosh Rathod <132924748+vikasrathod163@users.noreply.github.com>
2024-01-14 10:15:06 +0530

    Update task_1.java

--- task_1.java
+++ task_1.java
@@ -16,33 +16,33 @@ public class task_1{
 			System.out.println("Round : " + (i + 1));
 			int random_num = (int)(Math.random() * (max - min + 1) + min);
 
-			//System.out.println(b);
+			
 			System.out.println("Guess The Number Between 1 to 100 : ");
-			int attemtps = 0;
+			int attempts = 0;
 			boolean flag = true;
 
 			while (flag) {
-				while (attemtps < limit) {
+				while (attempts < limit) {
 					System.out.print("Enter The Number : ");
 					int num = sc.nextInt();
 
 					if (num == random_num) {
-						attemtps++;
+						attempts++;
 						System.out.println("Right Guess......");
-						System.out.println("Total Attemps : " + attemtps);
+						System.out.println("Total Attemps : " + attempts);
 						wins++;
 
 						flag = false;
 						break;
 					} else if (num > random_num) {
-						attemtps++;
+						attempts++;
 						System.out.println("too high .....");
 					} else {
-						attemtps++;
+						attempts++;
 						System.out.println("too low .....");
 					}
 				}
-				if (attemtps == limit && flag == true) {
+				if (attempts == limit && flag == true) {
 					System.out.println("You Lost...limit for attempts was " + limit);
 					flag = false;
 					break;
